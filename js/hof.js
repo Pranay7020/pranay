2844e87cc42283906f788a17f776b3d9120c8f97
Pranay7020 <pranayshinde872@gmail.com>
2025-09-09 11:03:40 +0530

    added new

--- js/hof.js
+++ js/hof.js
@@ -19,8 +19,7 @@ console.log(newArr);
 console.log(filterArr);
 
 //forEach method
-let newData=arr.forEach(Element,index,self)=>{
-    
+let newData=arr.forEach((Element,index,self)=>{
     console.log(index);
     console.log(self);
     console.log(Element);
@@ -28,4 +27,41 @@ let newData=arr.forEach(Element,index,self)=>{
         console.log(Element);
     }
 
-}
+});
+
+// reduce method
+let arr=[1,2,3,4,5,6,7,8,9,10];
+let val=arr.reduce((acc,cur)=>{
+    return acc+cur
+},0);
+console.log(val);
+
+// hof creates a reducer for summing a property
+const items=[
+    {name:'book',price:300},
+    {name:'pen',price:50},
+    {name:'notebook',price:150},
+];
+const createsumreducer=(prop)=>{
+    return (acc,obj)=>acc+obj[prop];
+};
+const total=items.reduce(createsumreducer('price'),0);
+console.log(total);// output: 500
+
+// fruits count
+const fruits=['apple','banana','apple','orange','banana','apple'];
+const fruitCount=fruits.reduce((acc,fruit)=>{
+    acc[fruit]=(acc[fruit]||0)+1;
+    return acc;
+},{});
+console.log(fruitCount);// output: {apple: 3, banana: 2, orange: 1}
+
+// letter count in a string
+const str='masaaii';
+const funcstr =str.split('');
+const countstr= funcstr.reduce((acc,string)=>{
+    acc[string]=(acc[string]||0)+1;
+    return acc;
+},{});
+console.log('-->countstr:',countstr);
+// output: {m: 1, a: 3, s: 1, i: 2}
---
+++ js/mathMethods.js
@@ -0,0 +1,32 @@
+// using Math methods
+let num =25;
+let ans = Math.pow(num,3);//cubic value // 15625
+console.log('-->ans:',ans);
+
+
+// square root method
+let number2= Math.sqrt(num);//root value //5
+
+// cubic root method
+let number3= Math.cbrt(num);//root value //2.924017738212866
+console.log('-->number3:',number3);
+
+// ceil method
+let number4= Math.ceil(4.2);//5
+let number5= Math.ceil(4.7);//5
+let number8= Math.ceil(-4.2);//-4
+let number9= Math.ceil(-4.7);//-4
+console.log('-->number5:',number5);
+console.log('-->number8:',number8);
+console.log('-->number9:',number9);
+
+
+// floor method
+let number6= Math.floor(4.2);//4
+let number7= Math.floor(4.7);//4
+let number10= Math.floor(-4.2);//-5
+let number11= Math.floor(-4.7);//-5
+console.log('-->number6:',number6);
+console.log('-->number7:',number7);
+console.log('-->number10:',number10);
+console.log('-->number11:',number11);
