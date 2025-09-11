3c2f9d9cc36d5b7b80809c9c8006eb343d64a6be
Pranay7020 <pranayshinde872@gmail.com>
2025-09-11 12:55:02 +0530

    added new

---
+++ js/function/f1question.js
@@ -0,0 +1,9 @@
+//Write a function that takes two numbers as input and returns their sum.
+let a = 5;
+let b = 10;
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(a, b));
---
+++ js/function/f3question.js
@@ -0,0 +1,6 @@
+//Create a function that converts Celsius to Fahrenheit.
+let temp_c = 30;
+function celsiusToFahrenheit(temp_c) {
+  return (temp_c * 9) / 5 + 32;
+}
+console.log("Temperature in Fahrenheit: " + celsiusToFahrenheit(temp_c));
