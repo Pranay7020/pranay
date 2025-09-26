aab2510d20f1695de48726b9e71891c15c5e430b
Pranay7020 <pranayshinde872@gmail.com>
2025-09-26 12:45:09 +0530

    added new

---
+++ js/practice_switch/swich.js
@@ -0,0 +1,15 @@
+// switch statement
+let fruit = "apple";
+switch (fruit) {
+    case "banana":
+        console.log("Banana is yellow.");
+        break;
+    case "apple":
+        console.log("Apple is red.");
+        break;
+    case "grape":
+        console.log("Grape is purple.");
+        break;
+    default:
+        console.log("Unknown fruit.");
+}
